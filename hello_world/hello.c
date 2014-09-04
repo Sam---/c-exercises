2b0f6219e178f7384b0ea5724cc78f433b2d0c6c
Samuel Phillips <samtheman@localhost>
2014-09-04 14:41:10 -0400

    Testing what the various escapes do

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -3,4 +3,29 @@
 main()
 {
     printf("hello, world\n");
+    printf("The escape code \"\\a\" does this: \a");
+    printf("The escape code \"\\b\" does this: \b");
+    printf("The escape code \"\\c\" does this: \c");
+    printf("The escape code \"\\d\" does this: \d");
+    printf("The escape code \"\\e\" does this: \e");
+    printf("The escape code \"\\f\" does this: \f");
+    printf("The escape code \"\\g\" does this: \g");
+    printf("The escape code \"\\h\" does this: \h");
+    printf("The escape code \"\\i\" does this: \i");
+    printf("The escape code \"\\j\" does this: \j");
+    printf("The escape code \"\\k\" does this: \k");
+    printf("The escape code \"\\l\" does this: \l");
+    printf("The escape code \"\\m\" does this: \m");
+    printf("The escape code \"\\n\" does this: \n");
+    printf("The escape code \"\\o\" does this: \o");
+    printf("The escape code \"\\p\" does this: \p");
+    printf("The escape code \"\\q\" does this: \q");
+    printf("The escape code \"\\s\" does this: \s");
+    printf("The escape code \"\\t\" does this: \t");
+    printf("The escape code \"\\u\" does this: \u");
+    printf("The escape code \"\\v\" does this: \v");
+    printf("The escape code \"\\w\" does this: \w");
+    printf("The escape code \"\\x\" does this: \x");
+    printf("The escape code \"\\y\" does this: \y");
+    printf("The escape code \"\\z\" does this: \z");
 }
