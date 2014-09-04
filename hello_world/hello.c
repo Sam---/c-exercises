b6b7b61750b38ff5982d070bdc2132a432279a31
Samuel Phillips <samtheman@localhost>
2014-09-04 14:46:24 -0400

    Added newlines!

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -3,13 +3,13 @@
 main()
 {
     printf("hello, world\n");
-    printf("The escape code \"\\a\" does this: \a");
-    printf("The escape code \"\\b\" does this: \b");
-    printf("The escape code \"\\e\" does this: \e");
-    printf("The escape code \"\\f\" does this: \f");
-    printf("The escape code \"\\n\" does this: \n");
-    printf("The escape code \"\\t\" does this: \t");
-//  printf("The escape code \"\\u\" does this: \u"); // Does unicode stuff
-    printf("The escape code \"\\v\" does this: \v");
-    printf("The escape code \"\\xff\" does this: \xff");
+    printf("The escape code \"\\a\" does this: \a\n");
+    printf("The escape code \"\\b\" does this: \b\n");
+    printf("The escape code \"\\e\" does this: \e\n");
+    printf("The escape code \"\\f\" does this: \f\n");
+    printf("The escape code \"\\n\" does this: \n\n");
+    printf("The escape code \"\\t\" does this: \t\n");
+//  printf("The escape code \"\\u\" does this: \u\n"); // Does unicode stuff
+    printf("The escape code \"\\v\" does this: \v\n");
+    printf("The escape code \"\\xff\" does this: \xff\n");
 }
