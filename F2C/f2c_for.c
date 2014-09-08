3e0840f9dc20b54466c1922bfd950c876022774b
Samuel Phillips <samtheman@localhost>
2014-09-08 14:43:37 -0400

    Changed f2c_for to use a function

--- F2C/f2c_for.c
+++ F2C/f2c_for.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+float celsius(float fahr);
+
 main()
 {
     int fahr;
 
     for (fahr = 300; fahr >= 0; fahr -= 20) {
-        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        printf("%3d %6.1f\n", fahr, celsius(fahr));
     }
+}
+
+float celsius(float fahr) {
+    return (5.0/9.0) * (fahr - 32.0);
 }
