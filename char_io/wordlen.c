dbf85dc3d2d89bfb4e4a2fa8ae145f882f1eda24
Samuel Phillips <samtheman@localhost>
2014-09-08 14:31:17 -0400

    Some changes

--- char_io/wordlen.c
+++ char_io/wordlen.c
@@ -4,21 +4,42 @@
 
 main()
 {
-    long word_len_count[MAXWORDLEN];
+    int word_len_count[MAXWORDLEN];
     int word_len = 0; // length of the current word
     int i;
+    int c;
+    int max = 0;
     
     for (i = 0; i < MAXWORDLEN; ++i) {
         word_len_count[i] = 0;
     }
 
     while ((c = getchar()) != EOF) {
-        if ( (c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z') ) {
+        if ( ('a' <= c && c <= 'z') || ( 'A' <= c && c <= 'Z') ) {
             word_len += 1;
         } else {
             word_len_count[word_len] += 1;
             word_len = 0;
         }
     }
+
+    for (i = 0; i < MAXWORDLEN; ++i) {
+        if (max < word_len_count[i]) {
+            max = word_len_count[i];
+        }
+    }
+
+    for (i = max; i < 0; --i) {
+        for (word_len = 0; word_len < MAXWORDLEN; ++word_len) {
+            if (word_len_count[word_len] >= i) {
+                putchar('|');
+            } else {
+                putchar(' ');
+            }
+        }
+        putchar('\n');
+    }
+
+}
 
     
