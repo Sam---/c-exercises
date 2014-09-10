a1dd767b54954aeabadd4a9a74ed80b4c102e3d3
Samuel Phillips <samtheman@localhost>
2014-09-10 14:20:41 -0400

    Attempted to add Unicode suppot to reverse.c, failed

--- getline/reverse.c
+++ getline/reverse.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 
 #define BUFMAX 2048
+#define UTF8MAXCHARS 4
+
+void reverse(char[], char[], size_t);
 
 int main()
 {
     int c;
     char line[BUFMAX];
+//  char utf8buf[UTF8MAXCHARS];
     int pos = 0;
+//  int u8chars = 0;
     int linecount = 0;
 
     while ((c = getchar()) != EOF) {
         if (c != '\n') {
             if (pos < BUFMAX) {
-                line[pos++] = c;
+//              if ((signed)c < 0) {
+//                  if (u8chars > UTF8MAXCHARS) {
+//                      fprintf(stderr, "Bad char on line %d", linecount);
+//                      return 2;
+//                  }
+//                  utf8buf[u8chars++] = c;
+//              } else {
+//                  if (u8chars > 0) {
+//                      reverse(line + pos, utf8buf, u8chars);
+//                      pos += u8chars;
+//                      u8chars = 0;
+//                  }
+                    line[pos++] = c;
+//              }
             } else {
                 fprintf(stderr, "Line too long: %d\n", linecount);
                 return 1;
@@ -32,3 +50,9 @@ int main()
     return 0;
 }
 
+void reverse(char dest[], char src[], size_t n) {
+    int dn = -1;
+    while (n > 0) {
+        dest[++dn] = src[--n];
+    }
+}
