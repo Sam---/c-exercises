b7beaed6977558f9dd6f3afa87fdf589d0affe0e
Samuel Phillips <samtheman@localhost>
2014-09-16 13:55:27 -0400

    Changed break.c

--- 1-20_1-24/break.c
+++ 1-20_1-24/break.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
 
+#define BUFMAX 2048
+
+#define INFO(str) fprintf(stdout, "\e[34m" str "\e[0m")
+
 int main()
 {
     char buffer[BUFMAX];
     int pos;
+    int col = 0;
+    int c;
 
     while ((c = getchar()) != EOF) {
-        switch (c) {
-            case ' ':
-            case '\t':
+        if (('a' <= c&&c <= 'z') || ('A' <= c&&c <= 'Z') ||
+            ('0' <= c&&c <= '9'))
+        {
+            if (pos <= BUFMAX) {
                 buffer[pos++] = c;
-                printf("%.*s", pos, buffer);
-                pos = 0;
-                break;
-            default:
-                buffer[pos++] = c;
+            } else {
+                printf("Word too long\n");
+                return;
+            }
+            ++col;
+        } else if (c == '\n') {
+            printf("%.*s\n", pos, buffer);
+            pos = col = 0;
+        } else {
+            if (col > 80) {
+                INFO("breaking");
+                pos = col = 0;
+                putchar('\n');
+            } else {
+                INFO("printing");
+                printf("%.*s%c", pos, buffer, c);
+                col +=1;
+            }
         }
     }
 
