d701ea8ac980bb21394e4810f3a7ed77b92bd11a
Samuel Phillips <samtheman@localhost>
2014-09-23 15:19:30 -0400

    Totally fixed

--- char_io/escape.c
+++ char_io/escape.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 
-main()
-{
-    int c;
+#define BUFMAX 1024
+#define BUF2MAX (BUFMAX * 2)
 
-    while ((c = getchar()) != EOF) {
-        switch (c) {
-            case '\t':
-                printf("\\t");
-                break;
-            case '\b':
-                printf("\\b");
-                break;
+/* 3-2 */
+size_t escape(char in[], char out[]) {
+    size_t inind = 0;
+    size_t outind = 0;
+
+    for (;in[inind]; inind++) {
+        switch (in[inind]) {
             case '\\':
-                printf("\\\\");
+                out[outind++] = '\\';
+                out[outind++] = '\\';
+                break;
+            case '\n':
+                out[outind++] = '\\';
+                out[outind++] = 'n';
+                break;
+            case '\t':
+                out[outind++] = '\\';
+                out[outind++] = 't';
                 break;
             default:
-                putchar(c);
+                out[outind++] = in[inind];
+                break;
         }
     }
+    out[outind] = '\0';
+    return outind;
+}
+
+main()
+{
+    char buf[BUFMAX];
+    char buf2[BUF2MAX];
+
+    while (fgets(buf, BUFMAX, stdin)) {
+        escape(buf, buf2);
+        printf("%s", buf2);
+    }
+
+    return 0;
 }
