f3733094a25fe3a934a0fc45945ec7799ee5e9c4
Samuel Phillips <samtheman@localhost>
2014-09-23 14:33:34 -0400

    Used the answer book.

--- bsearch/bsearch.c
+++ bsearch/bsearch.c
@@ -1,5 +1,19 @@
-/* 3-1 */
-int binsearch(int tgt, int v, int max) {
+#include <assert.h>
+#include <stdio.h>
+
+#define TASIZE (1024)
+
+void genrange(int arr[], int start, int max) {
+    int i;
+    for (i = start; i < max; i++) {
+        arr[i] = i;
+    }
+}
+
+/* 3-1 
+ * Dosen't actually work.
+ * */
+int binsearch(int tgt, int v[], int max) {
     int low = 0;
     int high = max- 1;
     int mid;
@@ -26,9 +40,9 @@ int exbinsearch(int tgt, int v[], int max) {
 
     while (low <= high) {
         mid = (low + high) / 2;
-        if (x < v[mid])
+        if (tgt < v[mid])
             high = mid - 1;
-        else if (x > v[mid])
+        else if (tgt > v[mid])
             low = mid + 1;
         else
             return mid;
@@ -36,9 +50,33 @@ int exbinsearch(int tgt, int v[], int max) {
     return -1;
 }
 
+int realbinsearch(int tgt, int v[], int max) {
+    int high, mid, low;
+    low = 0;
+    high = n - 1;
+    mid = (low+high) / 2;
+    while (low <= high && x != v[mid]) {
+        if (x < v[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    if (x== v[mid])
+        return mid;
+    else
+        return -1;
+}
+
 int main(int argc, char *argv[]) {
+    int testarr[TASIZE];
+
+    genrange(testarr, 0, TASIZE);
     switch (argc) {
         case 1:
-
-
-
+            printf("%d\n", binsearch(22, testarr, TASIZE) == 22);
+            break;
+        case 2:
+            printf("%d\n", exbinsearch(22, testarr, TASIZE) == 22);
+    }
+    return 0;
+}
