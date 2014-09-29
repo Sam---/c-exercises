039fa9eeeef4a44cae5bd5e99db9bf9dfaea1447
Samuel Phillips <samtheman@localhost>
2014-09-29 14:37:39 -0400

    Copyed in atof.c

---
+++ strings/atof.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+
+double myatof(char s[]) {
+    double val, power;
+    int i, sign;
+
+    for (i = 0; isspace(s[i]); i++) /* skip white space */
+        ;
+
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-') {
+        i++;
+    }
+    for (val = 0.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+    }
+    if (s[i] == '.') {
+        i++;
+    }
+    for (power = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    return sign * val / power;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        return EXIT_FAILURE;
+    }
+    printf("%g\n", myatof(argv[1]));
+    return EXIT_SUCCESS;
+}
