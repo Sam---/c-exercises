6f83a51e4cde5fd6386d4d020fc70a32971b369b
Samuel Phillips <samtheman@localhost>
2014-09-29 15:03:07 -0400

    Added -lm to compile.sh, temporary fix

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 
 git add "$@"
 git commit
-gcc "$@" -o "a.out" -std="c89" -ansi -pedantic -Werror -Wall -g -O1 -Wextra
+gcc "$@" -o "a.out" -std="c89" -lm \
+    -ansi -pedantic -Werror -Wall -g -O1 -Wextra
