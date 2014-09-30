6716d9beb8431d35815d0b494eee711bfae3095b
Samuel Phillips <samtheman@localhost>
2014-09-30 14:46:26 -0400

    First attempt at copying in the RPN calc

---
+++ rpncalc/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "getop.h"
+#include "stack.h"
+
+#include "main.h"
+
+/* RPN calculator */
+int main() {
+    int type;
+    double op2;
+    char oper[MAXOP];
+
+    while ((type = getop(oper)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(oper));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() + pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                opp2 = pop();
+                if (op2 != 0.0) {
+                    push(pop() / op2);
+                } else {
+                    printf("error: divide by zero\n");
+                }
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command \"%s\"\n", s);
+                break;
+        }
+    }
+    return EXIT_SUCCESS;
+}
