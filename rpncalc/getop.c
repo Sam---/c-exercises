68ed7c55a581018b3e452674b482cdc3cfa2b2b0
Samuel Phillips <samtheman@localhost>
2014-10-01 14:54:31 -0400

    Fixed major coding WTF problem

--- rpncalc/getop.c
+++ rpncalc/getop.c
@@ -5,18 +5,26 @@
 
 #include "getop.h"
 
+int isign(char c) {
+    if (isdigit(c) && c != '\n') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 /* getop: get next operator or numeric operand */
 int getop(char s[]) {
     int i = 1, c;
 
-    while ((c = getch()) != EOF && isspace(c));
+    while ((c = getch()) != EOF && isign(c));
     if (c == EOF) {
         return EOF;
     }
 
     s[0] = c;
 
-    while ((c = getch()) != EOF && !isspace(c)) {
+    while ((c = getch()) != EOF && !isign(c)) {
         s[i++] = c;
     }
     if (c == EOF) {
