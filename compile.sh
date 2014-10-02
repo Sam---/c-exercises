11c3730aa7bd7889392c2b40cd98367c36c5db9b
Samuel Phillips <samtheman@localhost>
2014-10-02 15:01:39 -0400

    Forgot to commit

--- compile.sh
+++ compile.sh
@@ -2,5 +2,6 @@
 
 git add "$@"
 git commit
-gcc "$@" -o "a.out" -std="c89" -lm \
+gcc "$@" \
+    -o "a.out" -std="c89" -lm -iquote'../sharedtools'\
     -ansi -pedantic -Werror -Wall -g -O1 -Wextra
