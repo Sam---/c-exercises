a7f124fb1798418d566c596274d1e22051d036b7
Samuel Phillips <samtheman@localhost>
2014-10-14 14:43:44 -0400

    I think it will work now

--- rpncalc/getop.c
+++ rpncalc/getop.c
@@ -9,6 +9,7 @@
 
 #include "getop.h"
 
+/*
 char *table[] = {
     "sin[e]", "cos[ine]", "tan[gent]",
     "a[rc]sin[e]", "a[rc]tan[gent]", "a[rc]cos[ine]0",
@@ -30,10 +31,12 @@ int getf(char s[]) {
     }
     return 0;
 }
+*/
 
 /* getop: get next operator or numeric operand 
  * returns 0 if EOL or error else B_*
  */
+/*
 int getop(char s[], size_t maxlen, double *num) {
     size_t tklen;
     int tkclass;
@@ -65,4 +68,91 @@ int getop(char s[], size_t maxlen, double *num) {
         return 0;
     }
 }
-
+*/
+int execop(char* s) {
+    double tmp;
+    switch (s[0]) {
+        case 's':
+            switch (s[1]) {
+                case 'i':
+                    push(sin(pop()));
+                    break;
+                case 'q':
+                    push(sqrt(pop()));
+                    break;
+                default:
+                    printf("Not found: \"%s\"\n", s);
+                    return 0;
+            }
+            break;
+        case 'c':
+            switch (s[1]) {
+                case 'o':
+                    push(cos(pop()));
+                    break;
+                case 'e':
+                    push(ceil(pop()));
+                    break;
+                default:
+                    printf("Not found: \"%s\"\n", s);
+                    return 0;
+            }
+            break;
+        case 't':
+            push(tan(pop()));
+            break;
+        case '^':
+            tmp = pop();
+            push(pow(pop(), tmp));
+            break;
+        case 'a':
+            switch (s[1]) {
+                case 's':
+                    push(asin(pop()));
+                    break;
+                case 'c':
+                    push(acos(pop()));
+                    break;
+                case 't':
+                    push(atan(pop()));
+                    break;
+                default:
+                    printf("Not found: \"%s\"\n", s);
+                    return 0;
+            }
+            break;
+        case 'h':
+            switch (s[1]) {
+                case 's':
+                    push(sinh(pop()));
+                    break;
+                case 'c':
+                    push(cosh(pop()));
+                    break;
+                case 't':
+                    push(tanh(pop()));
+                    break;
+                default:
+                    printf("Not found: \"%s\"\n", s);
+                    return 0;
+            }
+            break;
+        case 'l':
+            push(log(pop()));
+            break;
+        case 'f':
+            push(floor(pop()));
+            break;
+        case '%':
+            tmp = pop();
+            push(fmod(pop(), tmp));
+            break;
+        case '$':
+            push(getvar(s[1]));
+            break;
+        default:
+            printf("Not found: \"%s\"\n", s);
+            return 0;
+    }
+    return 1;
+}
