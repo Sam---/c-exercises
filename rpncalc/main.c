8e4df5902675da846c01d7a165938000422cefd1
Samuel Phillips <samtheman@localhost>
2014-10-14 14:38:48 -0400

    Attempt to compile new main.c

--- rpncalc/main.c
+++ rpncalc/main.c
@@ -3,66 +3,32 @@
 #include "shared.h"
 
 #include "getop.h"
+#include "tokenize.h"
 #include "stack.h"
 
 #include "main.h"
 
 /* RPN calculator */
 int main() {
-    int type;
-    double op2;
-    char oper[MAXOP];
-
-    while ((type = getop(oper)) != EOF) {
-        double v;
-        info("gotOp");
-        switch (type) {
-            case NUMBER:
-                info("number");
-                push(atof(oper));
-                break;
-            case NNUMBER:
-                info("nnumber");
-                push(-atof(oper));
-                break;
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() * pop());
-                break;
-            case '-':
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            case '/':
-                op2 = pop();
-                if (op2 != 0.0) {
-                    push(pop() / op2);
-                } else {
-                    printf("error: divide by zero\n");
-                }
-                break;
-            case '%':
-                op2 = pop();
-                v = pop() / op2;
-                push(v - (int)v);
-                break;
-            case 'e':
-                push(2.71828182845904523536028747135266249775724709369995957496696762772407663035354759457138217852516642742746);
-                break;
-            case '\n':
-                printf("\t%.8g\n", pop());
-                break;
-            case 'q':
-                info("quit");
-                return EXIT_SUCCESS;
-            default:
-                info("Err");
-                printf("error: unknown command \"%s\"\n", oper);
-                break;
+    size_t leng;
+    char buffer[CBMAX];
+    int tkclass;
+    while (leng = get_tokens(buffer, CBMAX, &tkclass)) {
+        if (tkclass == TK_WORD || tkclass == TK_SYMBOL) {
+            execop(buffer);
+        } else if (tkclass == TK_NUM) {
+            double num;
+            char *end;
+            errno = 0;
+            num = strtod(buffer, end);
+            if (errno == ERANGE) {
+                printf("num2big?\n");
+            }
+            push(num);
+        } else {
+            puts("What is \"%s\" suppsed to mean?\n");
         }
     }
-    info("returning");
+
     return EXIT_SUCCESS;
 }
