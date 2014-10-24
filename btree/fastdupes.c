a91db0a725f8bd5ad67a6cd3f8b19e3a50365647
Samuel Phillips <samtheman@localhost>
2014-10-24 12:30:56 -0400

    Removed something, added something

--- btree/fastdupes.c
+++ btree/fastdupes.c
@@ -1,6 +1,7 @@
-int *findnextdup(int list[], size_t listlen, int *buffer, size_t *bpos)
-    // precondition: buffer has enough memory to hold list
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int *findnextdup(int list[], size_t listlen, int *buffer, size_t *bpos) {
     int *end = list + listlen;
     for (int i = *list; list < end; list++) {
         size_t node = 0;
@@ -24,7 +25,10 @@ int *findnextdup(int list[], size_t listlen, int *buffer, size_t *bpos)
 
 int main(int argc, char *argv[]) {
     int *list = malloc((argc - 1) * sizeof(int));
-    int *buf =  malloc((argc - 1) * sizeof(int));
+    int *buf =  malloc(1024 * 1024); // should be enough
+    for (size_t i; i < argc; i++) {
+        list[i] = strtol(argv[i], NULL, 10);
+    }
     size_t bpos;
     for (int *next = list; next; next = findnextdup(next, argc, buf, &bpos)) {
         printf("Duplicate found: %d", *next);
