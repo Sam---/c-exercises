1d81d64b64f777bcc857c73e74989924539b87d0
Samuel Phillips <samtheman@localhost>
2014-11-17 12:55:14 -0500

    Recompiled shared.o for amd64 and make compile.sh executable

--- compile.sh
+++ compile.sh
@@ -1,12 +1,11 @@
-#/bin/sh
+#!/bin/sh
 
-git add "$@"
-lsinc -h -l "$@" | xargs git add
-git commit
+## git add "$@"
+## lsinc -h -l "$@" | xargs git add
+## git commit
 gcc \
     -o "a.out" -std="c89" \
     -iquote'../sharedtools' \
-    -ansi -pedantic -Werror -Wall -g -O1 -Wextra -Wno-parentheses \
-    -Wno-char-subscripts -lm $CFLAGS \
-    ../sharedtools/shared.o \
+    -ansi -pedantic -Werror -Wall -Wextra -Wno-parentheses \
+    -Wno-char-subscripts \
     "$@" 
