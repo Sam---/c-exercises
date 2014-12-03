2241324db6efc0c33f406853506283cc16564297
Samuel Phillips <samtheman@localhost>
2014-12-03 15:01:56 -0500

    Exercise 5-7
    (Also changed sizes to powers of 2, like $deity intended)

--- pointers/sort/sort.c
+++ pointers/sort/sort.c
@@ -5,11 +5,15 @@
 #include <string.h>
 #include "mygetline.h"
 
-#define MAXLINES 5000
+#define MAXLINES (5 * 1024)
+
+#define BUFLEN (10 * 1024 * 1024)
 
 char *lines[MAXLINES];
 
-size_t readlines(char *linesptr[], size_t nlines);
+size_t readlines(
+        char *lines[], size_t maxlines,
+        char *bigbuffer, size_t buflen);
 void writelines(char *linesptr[], size_t nlines);
 
 int compare(const void *a, const void *b) {
@@ -18,8 +22,9 @@ int compare(const void *a, const void *b) {
 
 int main() {
     size_t nlines;
+    static char bigbuffer[BUFLEN];
 
-    if ((nlines = readlines(lines, MAXLINES)) < SIZE_MAX) {
+    if ((nlines = readlines(lines, MAXLINES, bigbuffer, BUFLEN)) < SIZE_MAX) {
         qsort(lines, nlines, sizeof *lines, compare);
         writelines(lines, nlines);
         return 0;
@@ -29,17 +34,24 @@ int main() {
     }
 }
 
-#define MAXLEN 1000 /* Maximum length of a single line */
+#define MAXLEN 1024 /* Maximum length of a single line */
 
-size_t readlines(char *lines[], size_t maxlines) {
-    size_t len, nlines;
-    char *p, line[MAXLEN];
+size_t readlines(
+        char *lines[], size_t maxlines,
+        char *bigbuffer, size_t buflen
+    ) {
+    size_t len;
+    size_t used = 0;
+    size_t nlines = 0;
+    char line[MAXLEN];
+    char *p;
 
-    nlines = 0;
     while ((len = mygetline(line, MAXLEN)) > 0) {
-        if (nlines >= maxlines || (p = malloc(len)) == NULL) {
+        if (nlines >= maxlines || used + len > buflen) {
             return SIZE_MAX;
         } else {
+            p = &bigbuffer[used];
+            used += len;
             line[len-1] = '\0'; /* delete newline */
             strcpy(p, line);
             lines[nlines++] = p;
